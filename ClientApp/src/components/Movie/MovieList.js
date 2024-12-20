17d3bb822cc15f9cb377b0949ba4824fe672d487
humberto mori <humbertojacobm@gmail.com>
2024-12-20 18:03:14 -0500

    adding moving list

--- ClientApp/src/components/Movie/MovieList.js
+++ ClientApp/src/components/Movie/MovieList.js
@@ -1,13 +1,38 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
+import PropTypes from "prop-types";
 
-const MovieList = () => {
+const propTypes = {
+  movies: PropTypes.object.isRequired,
+};
+
+const MovieList = ({ movies }) => {
   return (
-    <ul>
-      <li>Movie 1</li>
-      <li>Movie 2</li>
-      <li>Movie 3</li>
-    </ul>
+    <div className="mt-4">
+      <Table striped bordered hover responsive>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Genre</th>
+            <th>Release Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => (
+            <tr key={movie.id}>
+              <td>{movie.id}</td>
+              <td>{movie.name}</td>
+              <td>{movie.genre}</td>
+              <td>{movie.releaseYear}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 };
+
+MovieList.propTypes = propTypes;
 
 export { MovieList };
